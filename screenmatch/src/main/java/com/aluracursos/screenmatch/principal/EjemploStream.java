df1fb33f419c71aa2992e5c325bf6bf0c12f35c2
daniela <dggtn97@gmail.com>
2024-10-12 20:24:34 -0300

    agrego funciones a las listas

--- screenmatch/src/main/java/com/aluracursos/screenmatch/principal/EjemploStream.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/principal/EjemploStream.java
@@ -8,6 +8,9 @@ public class EjemploStream {
         List<String> nombre= Arrays.asList("Brenda","Luis","Fernanda","Eric","Genesys");
         nombre.stream()
                 .sorted()
+                .limit(5)
+                .filter(n->n.startsWith("L"))
+                .map(n->n.toUpperCase())
                 .forEach(System.out::println);
 
     }
